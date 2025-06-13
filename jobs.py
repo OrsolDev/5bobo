71c7fbd91e86bafceae858bf97eb5820315e4941
Malik Kernel <dm372842@gmail.com>
2025-06-13 19:26:59 +0300

    ...

--- jobs.py
+++ jobs.py
@@ -1,5 +1,16 @@
 #CODE SOURCE BY MALIK FOR SCORPION CODING-------------------------
 
 #our first variable was a job name variable......never forget that.
-job="cleaner"
-print(job)
+job_name="cleaner"
+job_prix=300 #exemple le prix est en FC
+job_description="""
+Il s'agit de netoyer votre maison, vos vitre,vos chambre etc...
+"""
+job_mesure=1 #exemple ici ca veut dire 300FC pour netoyer 1 metre
+job_duration=1 #ici par exemple 1 munites pour netoyer 1 mettre
+
+print(f""""=========JOB===========
+Job name : {job_name} | Job prix : {job_prix}
+Job description : {job_description} | Job mesure : {job_mesure}
+Job duration : {job_duration}
+""")
